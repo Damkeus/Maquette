9ffea53425a9fb6d8a9489eb023bfa4c1d1baa1d
Damkeus <dylan.prochettemercure@ynov.com>
2024-09-05 16:43:09 +0200

    last one
    vcx

--- maquette.js
+++ maquette.js
@@ -1,3 +1,4 @@
+// Afficher l'interface PM et cacher la page d'accueil
 function showPMInterface() {
     document.querySelector('.profiles').classList.add('hidden');
     document.querySelector('.pm-interface').classList.remove('hidden');
@@ -8,6 +9,7 @@ function showPMInterface() {
     document.querySelector('.title').style.color = 'black';
 }
 
+// Retourner à la page d'accueil
 function showProfiles() {
     document.querySelector('.profiles').classList.remove('hidden');
     document.querySelector('.pm-interface').classList.add('hidden');
@@ -18,16 +20,17 @@ function showProfiles() {
     document.querySelector('.title').style.color = 'red';
 }
 
+// Afficher la section "Créer un chantier"
 function showCreateCh() {
     document.querySelector('.pm-interface').classList.add('hidden');
     document.querySelector('.create-ch').classList.remove('hidden');
     document.querySelector('.track-ch').classList.add('hidden');
     document.querySelector('.history-ch').classList.add('hidden');
-    document.querySelector()
     document.querySelector('.title').textContent = "Créer un Chantier";
     document.querySelector('.title').style.color = 'green';
 }
 
+// Afficher la section "Suivi de chantier"
 function showTrackCh() {
     document.querySelector('.pm-interface').classList.add('hidden');
     document.querySelector('.create-ch').classList.add('hidden');
@@ -37,6 +40,7 @@ function showTrackCh() {
     document.querySelector('.title').style.color = 'orange';
 }
 
+// Afficher la section "Historique de chantier"
 function showHistoryCh() {
     document.querySelector('.pm-interface').classList.add('hidden');
     document.querySelector('.create-ch').classList.add('hidden');
